f3880d7a919aa461ef0c2fd7684f0e14e3733842
vladfrangu <contact@skyra.pw>
2021-03-03 14:18:04 +0000

    build: type build for 1cfe4e7c804e62c142495686d2b83b81d0026c02

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,181 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const{getPromiseDetails:e,getProxyDetails:t}="undefined"==typeof process?{getPromiseDetails:()=>{},getProxyDetails:()=>{}}:process.binding("util");class Type{constructor(e,t=null){Object.defineProperty(this,"value",{enumerable:!0,configurable:!0,writable:!0,value:void 0}),Object.defineProperty(this,"is",{enumerable:!0,configurable:!0,writable:!0,value:void 0}),Object.defineProperty(this,"parent",{enumerable:!0,configurable:!0,writable:!0,value:void 0}),Object.defineProperty(this,"childKeys",{enumerable:!0,configurable:!0,writable:!0,value:new Map}),Object.defineProperty(this,"childValues",{enumerable:!0,configurable:!0,writable:!0,value:new Map}),this.value=e,this.is=Type.resolve(e),this.parent=t}get childTypes(){return this.childValues.size?`<${(this.childKeys.size?`${Type.list(this.childKeys)}, `:"")+Type.list(this.childValues)}>`:""}toString(){return this.check(),`${this.is}${this.childTypes}`}*parents(){let e=this;for(;e=e.parent;)yield e}isCircular(){for(const e of this.parents())if(e.value===this.value)return!0;return!1}addValue(e){const t=new Type(e,this);this.childValues.set(t.is,t)}addEntry([e,t]){const i=new Type(e,this);this.childKeys.set(i.is,i),this.addValue(t)}check(){if(Object.isFrozen(this))return;const i=e(this.value),s=t(this.value);if("object"==typeof this.value&&this.isCircular())this.is=`[Circular:${this.is}]`;else if(i&&i[0])this.addValue(i[1]);else if(s&&s[0])this.is="Proxy",this.addValue(s[0]);else if(this.value instanceof Map)for(const e of this.value)this.addEntry(e);else if(Array.isArray(this.value)||this.value instanceof Set)for(const e of this.value)this.addValue(e);else if("Object"===this.is){this.is="Record";for(const e of Object.entries(this.value))this.addEntry(e)}Object.freeze(this)}static resolve(e){const t=typeof e;switch(t){case"object":return null===e?"null":e.constructor&&e.constructor.name;case"function":return`${e.constructor.name}(${e.length}-arity)`;case"undefined":return"void";default:return t}}static list(e){return[...e.values()].sort().join(" | ")}}exports.Type=Type;
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const { getPromiseDetails, getProxyDetails } = typeof process === 'undefined'
+    ? {
+        getPromiseDetails: () => undefined,
+        getProxyDetails: () => undefined
+    }
+    : // @ts-ignore process.binding exists
+        process.binding('util');
+/**
+ * The class for deep checking Types
+ */
+class Type {
+    /**
+     * @param value The value to generate a deep Type of
+     * @param parent The parent value used in recursion
+     */
+    constructor(value, parent = null) {
+        /**
+         * The value to generate a deep Type of
+         */
+        Object.defineProperty(this, "value", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
+        /**
+         * The shallow type of this
+         */
+        Object.defineProperty(this, "is", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
+        /**
+         * The parent of this Type
+         */
+        Object.defineProperty(this, "parent", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
+        /**
+         * The child keys of this Type
+         */
+        Object.defineProperty(this, "childKeys", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: new Map()
+        });
+        /**
+         * The child values of this Type
+         */
+        Object.defineProperty(this, "childValues", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: new Map()
+        });
+        this.value = value;
+        this.is = Type.resolve(value);
+        this.parent = parent;
+    }
+    /**
+     * The type string for the children of this Type
+     */
+    get childTypes() {
+        if (!this.childValues.size)
+            return '';
+        return `<${(this.childKeys.size ? `${Type.list(this.childKeys)}, ` : '') + Type.list(this.childValues)}>`;
+    }
+    /**
+     * The full type string generated.
+     */
+    toString() {
+        this.check();
+        return `${this.is}${this.childTypes}`;
+    }
+    /**
+     * Walks the linked list backwards, for checking circulars.
+     */
+    *parents() {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias, no-cond-assign, consistent-this
+        let current = this;
+        // eslint-disable-next-line no-cond-assign
+        while ((current = current.parent))
+            yield current;
+    }
+    /**
+     * Checks if the value of this Type is a circular reference to any parent.
+     */
+    isCircular() {
+        for (const parent of this.parents())
+            if (parent.value === this.value)
+                return true;
+        return false;
+    }
+    /**
+     * The subtype to create based on this.value's sub value.
+     * @param value The sub value
+     */
+    addValue(value) {
+        const child = new Type(value, this);
+        this.childValues.set(child.is, child);
+    }
+    /**
+     * The subtype to create based on this.value's entries.
+     * @param entry The entry
+     */
+    addEntry([key, value]) {
+        const child = new Type(key, this);
+        this.childKeys.set(child.is, child);
+        this.addValue(value);
+    }
+    /**
+     * Get the deep type name that defines the input.
+     */
+    check() {
+        if (Object.isFrozen(this))
+            return;
+        const promise = getPromiseDetails(this.value);
+        const proxy = getProxyDetails(this.value);
+        if (typeof this.value === 'object' && this.isCircular()) {
+            this.is = `[Circular:${this.is}]`;
+        }
+        else if (promise && promise[0]) {
+            this.addValue(promise[1]);
+        }
+        else if (proxy && proxy[0]) {
+            this.is = 'Proxy';
+            this.addValue(proxy[0]);
+        }
+        else if (this.value instanceof Map) {
+            for (const entry of this.value)
+                this.addEntry(entry);
+        }
+        else if (Array.isArray(this.value) || this.value instanceof Set) {
+            for (const value of this.value)
+                this.addValue(value);
+        }
+        else if (this.is === 'Object') {
+            this.is = 'Record';
+            for (const entry of Object.entries(this.value))
+                this.addEntry(entry);
+        }
+        Object.freeze(this);
+    }
+    /**
+     * Resolves the type name that defines the input.
+     * @param value The value to get the type name of
+     */
+    static resolve(value) {
+        const type = typeof value;
+        switch (type) {
+            case 'object':
+                return value === null ? 'null' : value.constructor && value.constructor.name;
+            case 'function':
+                return `${value.constructor.name}(${value.length}-arity)`;
+            case 'undefined':
+                return 'void';
+            default:
+                return type;
+        }
+    }
+    /**
+     * Joins the list of child types.
+     * @param values The values to list
+     */
+    static list(values) {
+        return [...values.values()].sort().join(' | ');
+    }
+}
+
+exports.Type = Type;
 //# sourceMappingURL=index.js.map
